3d899b7663da075b2e14a57a6f28a08e88227763
Doug Crescenzi <doug@telapi.com>
2013-07-04 14:35:01 -0400

    added list and get functions to MMS; both still require tests

--- lib/telapi/mms.rb
+++ lib/telapi/mms.rb
@@ -4,6 +4,25 @@ module Telapi
     class << self
       MAX_BODY_LENGTH = 160
 
+      # Returns a resource collection containing Telapi::MMS objects
+      #
+      # Optional params is a hash containing:
+      # +To+:: mobile phone number
+      # +From+:: TelAPI or mobile phone number
+      # +DateSent+:: date in the following format: YYYY-MM-DD
+      # +Page+:: integer greater than 0
+      # +PageSize+:: integer greater than 0
+      def list(optional_params = {})
+        response = Network.get(['MMS', 'Messages'], optional_params)
+        ResourceCollection.new(response, 'mms_messages', self)
+      end
+
+      # Returns a specific Telapi::Message object given its id
+      def get(id)
+        response = Network.get(['MMS', 'Messages', id])
+        Message.new(response)
+      end
+
       # Creates an MMS Message, returning a Telapi::MMS object
       #
       # Required params:
@@ -15,9 +34,9 @@ module Telapi
       # Optional param:
       # +status_callback+:: valid URL
 
-      def create(from, to, body, attachment, status_callback=nil)
+      def create(to, from, body, attachment, status_callback=nil)
         raise RangeError, "Body must be between 0 and #{MAX_BODY_LENGTH} characters" if body.length>MAX_BODY_LENGTH || body.length==0
-        opts = { :From => from, :To => to, :Body => body, :Attachment => attachment, :StatusCallback => status_callback }
+        opts = { :To => to, :From => from, :Body => body, :Attachment => attachment, :StatusCallback => status_callback }
         response = Network.post(['MMS', 'Messages'], opts)
         MMS.new(response)
       end
